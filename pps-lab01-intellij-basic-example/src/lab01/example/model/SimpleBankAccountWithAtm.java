6d35957de0b8507469b5df81f2ace231d7b28a61
Sofy24 <sofia.tosi4@studio.unibo.it>
2023-02-27 10:37:41 +0100

    exercise 1

---
+++ pps-lab01-intellij-basic-example/src/lab01/example/model/SimpleBankAccountWithAtm.java
@@ -0,0 +1,48 @@
+package lab01.example.model;
+
+public class SimpleBankAccountWithAtm implements BankAccount {
+
+    private double balance;
+    private final AccountHolder holder;
+    private static final int FEE = 1;
+
+    public SimpleBankAccountWithAtm(AccountHolder holder, int balance) {
+        this.holder = holder;
+        this.balance = balance;
+    }
+
+    @Override
+    public AccountHolder getHolder() {
+        return this.holder;
+    }
+
+    @Override
+    public double getBalance() {
+        return this.balance;
+    }
+
+    @Override
+    public void deposit(int userID, double amount) {
+        if (checkUser(userID)) {
+            this.balance += amount;
+            this.balance -= SimpleBankAccountWithAtm.FEE;
+        }
+    }       
+
+    @Override
+    public void withdraw(int userID, double amount) {
+        if (checkUser(userID) && isWithdrawAllowed(amount)) {
+            this.balance -= amount;
+            this.balance -= SimpleBankAccountWithAtm.FEE;
+        }
+    }
+
+    private boolean isWithdrawAllowed(final double amount){
+        return this.balance >= amount;
+    }
+
+    private boolean checkUser(final int id) {
+        return this.holder.getId() == id;
+    }
+
+}
--- pps-lab01-intellij-basic-example/test/SimpleBankAccountTest.java
+++ pps-lab01-intellij-basic-example/test/SimpleBankAccountTest.java
@@ -1,6 +1,7 @@
 import lab01.example.model.AccountHolder;
 import lab01.example.model.BankAccount;
 import lab01.example.model.SimpleBankAccount;
+import lab01.example.model.SimpleBankAccountWithAtm;
 
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,16 +13,19 @@ class SimpleBankAccountTest {
 
     private AccountHolder accountHolder;
     private BankAccount bankAccount;
+    private BankAccount bankAccountWithAtm;
 
     @BeforeEach
     void beforeEach(){
         accountHolder = new AccountHolder("Mario", "Rossi", 1);
         bankAccount = new SimpleBankAccount(accountHolder, 0);
+        bankAccountWithAtm = new SimpleBankAccountWithAtm(accountHolder, 0);
     }
 
     @Test
     void testInitialBalance() {
         assertEquals(0, bankAccount.getBalance());
+        assertEquals(0, bankAccountWithAtm.getBalance());
     }
 
     @Test
@@ -49,5 +53,13 @@ class SimpleBankAccountTest {
         bankAccount.deposit(accountHolder.getId(), 100);
         bankAccount.withdraw(2, 70);
         assertEquals(100, bankAccount.getBalance());
+    }
+
+    @Test
+    void testSimpleBankAccountWithAtm() {
+        bankAccountWithAtm.deposit(accountHolder.getId(), 30);
+        assertEquals(29, bankAccountWithAtm.getBalance());
+        bankAccountWithAtm.withdraw(accountHolder.getId(), 19);
+        assertEquals(9, bankAccountWithAtm.getBalance());
     }
 }
